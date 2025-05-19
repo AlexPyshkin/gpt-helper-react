7d5f29e5d149863dd39f965c82ea3245a2978da7
AlexanderPyshkin <alexander.pyshkin@include.ai>
2025-05-19 14:42:37 +0400

    fix nginx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, from } from '@apollo/client';
+import { onError } from "@apollo/client/link/error";
 import App from "./App.tsx";
 import './index.css';
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.error(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.error(`[Network error]: ${networkError}`);
+});
 
 const httpLink = createHttpLink({
   uri: '/graphql',
@@ -10,7 +21,7 @@ const httpLink = createHttpLink({
 });
 
 const client = new ApolloClient({
-  link: httpLink,
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
